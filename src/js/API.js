82d659714f766c9d36aaf014357348cc7370fa6a
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-11-18 20:27:58 +0200

    .

--- src/js/API.js
+++ src/js/API.js
@@ -1,5 +1,8 @@
 import renderEvents from "./main";
 
+const eventsContainer = document.getElementById('events');
+
+
 const BASEURL = 'https://app.ticketmaster.com/discovery/v2/events?';
 const API = 'apikey=frHax5AbrwLC1IuSbtMAkrrAQfOByT93';
 
@@ -16,5 +19,27 @@ async function fetchEvents() {
   }
 }
 
-fetchEvents();
+
+async function getEventByCountry(countryCode){
+  try {
+   const url = `${BASEURL}${API}&countryCode=${countryCode}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log('error');
+  }
+}
+
+getEventByCountry('')
+.then((data)=>{
+  if (data._embedded) {
+    renderEvents(data._embedded.events)
+
+  } else {
+    eventsContainer.innerHTML = `<p>No events found for country: ${countryCode}</p>`
+  }
+})
+
+// fetchEvents();
 export default fetchEvents
