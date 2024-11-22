1a07d1b8d9ea682d87080081754d663847be3805
34x340 <instwertt@gmail.com>
2024-11-22 20:11:04 +0200

    modal+footer

--- src/partials/js/modal.js
+++ src/partials/js/modal.js
@@ -44,66 +44,45 @@ const eventsData = [
       who: "Monatik",
       priceStandard: "Standart 499 UAH",
       priceVIP: "VIP 999 UAH"
-    },
-<<<<<<< Updated upstream
-    {
-      id: 5,
-      info: "MONATIK LOVE IT RYTHM",
-      whenDate: "2021-07-17",
-      whenTime: "18:00 (Kyiv/Ukraine)",
-      whereCity: "Kyiv, Ukraine",
-      whereLocation: "VDNH",
-      who: "Monatik",
-      priceStandard: "Standart 499 UAH",
-      priceVIP: "VIP 999 UAH"    
-    },
-=======
->>>>>>> Stashed changes
-  ];
+    },]
 
-const modal = document.getElementById("modal");
-const closeBtn = document.getElementById("close-btn");
-
-const infoText = document.getElementById("modal-info-text");
-const whenDate = document.getElementById("modal-when-date");
-const whenTime = document.getElementById("modal-when-time");
-const whereCity = document.getElementById("modal-where-city");
-const whereLocation = document.getElementById("modal-where-location");
-const whoText = document.getElementById("modal-who");
-const priceStandard = document.getElementById("modal-price-standard");
-const priceVIP = document.getElementById("modal-price-vip");
-
-
-
-function openModal(eventId) {
-    const eventData = eventsData.find(event => event.id === eventId);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-if (eventData) {
-    infoText.textContent = eventData.info;
-    whenDate.textContent = eventData.whenDate;
-    whenTime.textContent = eventData.whenTime;
-    whereCity.textContent = eventData.whereCity;
-    whereLocation.textContent = eventData.whereLocation;
-    whoText.textContent = eventData.who;
-    priceStandard.textContent = eventData.priceStandard;
-    priceVIP.textContent = eventData.priceVIP;
+    const modal = document.getElementById("modal");
+    const closeBtn = document.getElementById("close-btn");
     
-    modal.style.display = "block";
-  }
-}
-<<<<<<< Updated upstream
-=======
+    const infoText = document.getElementById("modal-info-text");
+    const whenDate = document.getElementById("modal-when-date");
+    const whenTime = document.getElementById("modal-when-time");
+    const whereCity = document.getElementById("modal-where-city");
+    const whereLocation = document.getElementById("modal-where-location");
+    const whoText = document.getElementById("modal-who");
+    const priceStandard = document.getElementById("modal-price-standard");
+    const priceVIP = document.getElementById("modal-price-vip");
+    
+    
+    
+    function openModal(eventId) {
+        const eventData = eventsData.find(event => event.id === eventId);
+    if (eventData) {
+        infoText.textContent = eventData.info;
+        whenDate.textContent = eventData.whenDate;
+        whenTime.textContent = eventData.whenTime;
+        whereCity.textContent = eventData.whereCity;
+        whereLocation.textContent = eventData.whereLocation;
+        whoText.textContent = eventData.who;
+        priceStandard.textContent = eventData.priceStandard;
+        priceVIP.textContent = eventData.priceVIP;
+        
+        modal.style.display = "block";
+      }
+    }
+    function closeModal(event) {
+      if (!event || event.target === document.getElementById('modal')) {
+        document.getElementById('modal').style.display = 'none';
+        document.removeEventListener('keydown', handleEscape);
+      }
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    }
+    
 
-function closeModal(event) {
-  if (!event || event.target === document.getElementById('modal')) {
-    document.getElementById('modal').style.display = 'none';
-    document.removeEventListener('keydown', handleEscape);
-  }
-  if (event.key === 'Escape') {
-    closeModal();
-  }
-}
->>>>>>> Stashed changes
