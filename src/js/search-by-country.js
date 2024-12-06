74925fc0def011c168b557376734bb2c61c74044
Iryna <irasokolova25@ukr.net>
2024-12-06 20:07:39 +0200

    Update search-by-country.js

--- src/js/search-by-country.js
+++ src/js/search-by-country.js
@@ -1,9 +1,30 @@
 import {getEventByCountry, getEventBySearchKey} from './API';
 import renderEvents from './main';
-import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 
-const searchInput = document.getElementById("searchInput")
+const searchInput = document.getElementById("searchInputSel")
 const  countrySelect = document.getElementById("countrySelect")
+
+
+    countrySelect.addEventListener("change", (event)=>{
+        const country = event.currentTarget.value.toLowerCase();
+        getEventByCountry(country)
+        .then((events)=>{
+            console.log(events);
+            renderEvents(events)
+        })
+    
+    })  
+    
+searchInput.addEventListener("input",
+       debounce( function (event) {
+            const keyword = event.target.value.toLowerCase();
+            getEventBySearchKey(keyword)
+            .then(events=>{
+                renderEvents(events);
+            })
+        }, 500)
+    );
 
 
 function searchByCountry() {
@@ -65,29 +86,4 @@ function autocomplete(input, arr){
             }
         });
     });}
-
-
-    countrySelect.addEventListener("change", (event)=>{
-        const country = event.currentTarget.value.toLowerCase();
-        getEventByCountry(country)
-        .then((events)=>{
-            console.log(events);
-            renderEvents(events)
-        })
-    
-    })   
-
-
-    document.getElementById("searchInputSel").addEventListener("input",
-       throttle( function (event) {
-            console.log('Madonna');
-            const keyword = event.currentTarget.value.toLowerCase();
-            getEventBySearchKey(keyword)
-            .then(events=>{
-                renderEvents(events);
-            })
-        }, 500)
-    );
-
-
 
